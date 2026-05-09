067e0e40ac09caedafb7b0c0bbedb214e517d3ff
AgenteBP <104331234+AgenteBP@users.noreply.github.com>
2026-05-09 15:40:20 -0300

    9/05/2026
    Subiendo proyecto al main

---
+++ src/main/java/com/ChallengeSpringBoot/SistemaGestionDeProyectos/SistemaGestionDeProyectosApplication.java
@@ -0,0 +1,13 @@
+package com.ChallengeSpringBoot.SistemaGestionDeProyectos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaGestionDeProyectosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaGestionDeProyectosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ChallengeSpringBoot/SistemaGestionDeProyectos/SistemaGestionDeProyectosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ChallengeSpringBoot.SistemaGestionDeProyectos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaGestionDeProyectosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
